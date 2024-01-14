58d428007a0420aa1e50e7001b61f66fe8e21c7e
t3hc0nnect10n <43529393+t3hc0nnect10n@users.noreply.github.com>
2024-01-14 21:14:03 +0300

    Add files via upload

---
+++ RemovableDiskDenyWriteAccess.ps1
@@ -0,0 +1,33 @@
+﻿# Описание, как использовать try_catch_finally блоки для обработки завершающих ошибок:
+# https://learn.microsoft.com/ru-ru/powershell/module/microsoft.powershell.core/about/about_try_catch_finally?view=powershell-7.3
+
+try
+{
+	# Ветка в реестре
+	$regkey='HKLM:\Software\Policies\Microsoft\Windows\RemovableStorageDevices\{53f5630d-b6bf-11d0-94f2-00a0c91efb8b}'
+	# Проверка на наличии ветки в реестре
+	$exists = Test-Path $regkey
+
+	try
+	{
+		if (!$exists) {
+			# Если ветки в реестре нет, то она создастся с присвоенным классом устройства "{53f5630d-b6bf-11d0-94f2-00a0c91efb8b}"
+			New-Item -Path 'HKLM:\Software\Policies\Microsoft\Windows\RemovableStorageDevices' -Name '{53f5630d-b6bf-11d0-94f2-00a0c91efb8b}' -Force | Out-Null
+			# Запрет на запись
+			New-ItemProperty -Path $regkey -Name 'Deny_Write' -Value 1 -PropertyType 'DWord' -Force | Out-Null
+		}
+	}
+    finally
+    {
+        # Индикация успешновыполненого скритпта
+        Write-Host ""
+        Write-Host "GOOD" -BackgroundColor Green
+    }
+}
+catch
+{
+    # Вывод шибки в консоль
+    Write-Host ""
+    Write-Host "ERROR" -BackgroundColor Red
+    Write-Host "Error: $($_.Exception.Messege)"
+}
