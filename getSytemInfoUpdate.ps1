99d04569acdf0c8c528a6e4f3f54efd86d629152
t3hc0nnect10n <43529393+t3hc0nnect10n@users.noreply.github.com>
2024-04-01 13:12:45 +0300

    Update getSytemInfoUpdate.ps1

--- getSytemInfoUpdate.ps1
+++ getSytemInfoUpdate.ps1
@@ -21,4 +21,6 @@ Get-WinEvent -filterHashtable @{ LogName = 'Setup'; Id = 2 }| Where-Object {$_.M
 #expand -f:* "C:\Temp\windows10.0-kb5034768-x64_04b794598371fdc01bb5840c68487388ca029ad5.msu" C:\Temp\KB5034768\x64\
 #
 #Установка CAB файла, пример: 
-#DISM.exe /Online /Add-Package /PackagePath:C:\Temp\KB5034768\x64\Windows10.0-KB5034768-x64.cab		
+#DISM.exe /Online /Add-Package /PackagePath:C:\Temp\KB5034768\x64\Windows10.0-KB5034768-x64.cab
+$shell = New-Object -ComObject Wscript.Shell
+$shell.popup("Ура все получилось",0,"Результат" , 64)
