dd38fb3b62d1c8075ae1b5c23e6e3a3133c4f604
t3hc0nnect10n <43529393+t3hc0nnect10n@users.noreply.github.com>
2024-05-20 12:50:46 +0300

    Add files via upload

---
+++ GetDynamicDistributionGroupMember.ps1
@@ -0,0 +1,19 @@
+﻿# Задаём переменной '$Name' имя файла 'DynamicDistributionGroup' в текстовом формате 'txt'
+$Name_DD = 'DynamicDistributionGroup.txt'
+
+# Задаём переменной '$Path' путь, в котором будет сохраняться полученный файл 'DynamicDistributionGroup.txt' 
+$Path = '<Указываем полный путь где будет храниться файл>'
+
+Get-DynamicDistributionGroup | ForEach-Object {
+    foreach ($recipient in Get-Recipient -RecipientPreviewFilter $_.RecipientFilter -ResultSize Unlimited) {
+        [pscustomobject]@{
+            DistributionGroup  = $_.Name
+            Recipient          = $recipient.Name
+            PrimarySmtpAddress = $recipient.PrimarySmtpAddress
+            EmailAddresses     = $recipient.EmailAddresses
+            Title              = $recipient.Title
+            Department         = $recipient.Department
+            Company            = $recipient.Company
+        } 
+    }
+} | Export-CSV '<Указываем полный путь где будет храниться файл>\CSV_DynamicDistributionGroupMember.csv' -NoTypeInformation -Encoding UTF8
