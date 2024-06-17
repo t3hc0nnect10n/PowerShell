88b5cf986f0f4fedeaaf6064828b43f6c15a72bc
t3hc0nnect10n <43529393+t3hc0nnect10n@users.noreply.github.com>
2024-06-17 13:37:41 +0300

    Add files via upload

---
+++ DisabledAndMoveUser.ps1
@@ -0,0 +1,34 @@
+﻿# Задаём переменной $ListUsers полный путь хранения фала. 
+# Например: C:\Users\Ivan\Documents\DisabledUsers.txt
+$ListUsers = Read-Host "Введите путь"
+
+# Переменной $BlockedUsers присвоен distinguishedName контейнера, в котором хранятся отключенные учетные записи.
+# Например: OU=Deactivated Users,DC=example,DC=local
+$BlockedUsers = Read-Host "Введите distinguishedName контейнера"
+
+# Удаление лишних пробелов
+$Users = Get-Content $ListUsers.Trim()
+
+echo " "
+Write-Host $Users.Count "Учётных записей" -ForegroundColor Yellow
+echo " "
+
+foreach ($User in $Users) {
+    
+    # Перемещение пользователя в контейнер OU=Blocked Users
+    Get-ADUser $User | Move-ADObject -TargetPath $BlockedUsers
+    
+    # Отключение учетной записи
+    Disable-ADAccount -Identity $User
+
+    # Переменная $Check проверяет на наличие отключенной учетной записи в контейнере OU=Blocked Users 
+    $Check = Get-ADUser -Filter * -SearchBase $BlockedUsers -Properties * | Where-Object {$_.Enabled -like $false -and $_.SamAccountName -like $User}
+    
+    # Переменная $Name выводит в консоль ФИО в виде массива
+    $Name = Get-ADUser $User -Properties * | Select-Object Name
+
+    # Условие проверки на выполнение отключения учетной записи и перемещение в контейнер OU=Blocked Users, 
+    # Если условие истинно, то в консоль выводится ФИО и SamAccountName в зелёном цвете, если нет, то в красном. 
+    if ($Check) {Write-Host $Name $User "заблокирован" -ForegroundColor Green} else {Write-Host $Name $User "не заблокирован" -ForegroundColor Red}
+
+}
