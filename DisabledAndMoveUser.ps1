b396088bdc2af84a080094106745c204de32b66e
t3hc0nnect10n <43529393+t3hc0nnect10n@users.noreply.github.com>
2024-06-18 09:48:09 +0300

    Update DisabledAndMoveUser.ps1

--- DisabledAndMoveUser.ps1
+++ DisabledAndMoveUser.ps1
@@ -1,9 +1,11 @@
-﻿# Задаём переменной $ListUsers полный путь хранения фала. 
+# Задаём переменной $ListUsers полный путь хранения фала. 
 # Например: C:\Users\Ivan\Documents\DisabledUsers.txt
+echo " "
 $ListUsers = Read-Host "Введите путь"
 
 # Переменной $BlockedUsers присвоен distinguishedName контейнера, в котором хранятся отключенные учетные записи.
 # Например: OU=Deactivated Users,DC=example,DC=local
+echo " "
 $BlockedUsers = Read-Host "Введите distinguishedName контейнера"
 
 # Удаление лишних пробелов
@@ -15,12 +17,12 @@ echo " "
 
 foreach ($User in $Users) {
     
-    # Перемещение пользователя в контейнер OU=Blocked Users
+    # Перемещение пользователя в контейнер заданной переменной $BlockedUsers
     Get-ADUser $User | Move-ADObject -TargetPath $BlockedUsers
     
     # Отключение учетной записи
     Disable-ADAccount -Identity $User
-
+    
     # Переменная $Groups получает все группы безопасности пользователя кроме "Пользователи домена"
     $Groups = Get-ADPrincipalGroupMembership -Identity $User | Where {$_.Name -ne "Пользователи домена"}
     
@@ -30,14 +32,18 @@ foreach ($User in $Users) {
         Remove-ADGroupMember -identity $Group.name -Members $User -Confirm:$false
     }
 
-    # Переменная $Check проверяет на наличие отключенной учетной записи в контейнере OU=Blocked Users 
-    $Check = Get-ADUser -Filter * -SearchBase $BlockedUsers -Properties * | Where-Object {$_.Enabled -like $false -and $_.SamAccountName -like $User}
-    
     # Переменная $Name выводит в консоль ФИО в виде массива
     $Name = Get-ADUser $User -Properties * | Select-Object Name
+    
+    # Переменная $Check проверяет на наличие отключенной учетной записи в контейнере заданной переменной $BlockedUsers
+    $CheckUser = Get-ADUser -Filter * -SearchBase $BlockedUsers -Properties * | Where-Object {$_.Enabled -like $false -and $_.SamAccountName -like $User}
+    
+    # Переменная $CheckGroup получает список групп, в которые входит пользователь
+    $CheckGroup = Get-ADPrincipalGroupMembership -Identity $User | Select-Object Name
 
-    # Условие проверки на выполнение отключения учетной записи и перемещение в контейнер OU=Blocked Users, 
-    # Если условие истинно, то в консоль выводится ФИО и SamAccountName в зелёном цвете, если нет, то в красном. 
-    if ($Check) {Write-Host $Name $User "заблокирован" -ForegroundColor Green} else {Write-Host $Name $User "не заблокирован" -ForegroundColor Red}
+    # Условие проверки на выполнение отключения учетной записи, наличие группы безопасности "Пользователи домена" и перемещение в контейнер заданной переменной $BlockedUsers, 
+    # Если условие истинно, то в консоль выводится ФИО, SamAccountName и группы безопасности в зелёном цвете, если нет, то в красном. 
+    if ($CheckUser) {Write-Host $Name $User "- заблокирован. Группы доступа:" $CheckGroup -ForegroundColor Green} 
+    else {Write-Host $Name $User "- не заблокирован. Группы доступа:" $CheckGroup -ForegroundColor Red}
 
 }
