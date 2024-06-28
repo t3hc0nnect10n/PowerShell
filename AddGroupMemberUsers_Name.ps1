08677160567a4cfda9191b2ede7467def33b4cc6
t3hc0nnect10n <43529393+t3hc0nnect10n@users.noreply.github.com>
2024-06-28 09:11:02 +0300

    Update AddGroupMemberUsers_Name.ps1

--- AddGroupMemberUsers_Name.ps1
+++ AddGroupMemberUsers_Name.ps1
@@ -165,7 +165,6 @@ function CreateUsersList() {
 
     Write-Host " Переменной 'ListUsers' задаём список из пользователей по ФИО." -ForegroundColor Yellow
     $ListUsers = [System.Collections.ArrayList]@()
-
     [int]$UsersNumbers = Read-Host " Введите количество пользователей"
     
     echo " "
