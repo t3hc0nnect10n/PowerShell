cb36b2477e8cae0ded7bb5d3a7432ae01c4bb082
t3hc0nnect10n <43529393+t3hc0nnect10n@users.noreply.github.com>
2025-01-16 18:37:34 +0300

    Update RemovableDiskDenyWriteAccess.ps1

--- RemovableDiskDenyWriteAccess.ps1
+++ RemovableDiskDenyWriteAccess.ps1
@@ -1,27 +1,27 @@
 <#
-	Название параметра GPO              Подветка с именем Device Class GUID           Имя параметра реестра:
+	Название параметра GPO              Подветка с именем Device Class GUID             Имя параметра реестра:
 
 	Floppy Drives:
-	Deny read access                    {53f56311-b6bf-11d0-94f2-00a0c91efb8b}        Deny_Read
-	Deny write access                   {53f56311-b6bf-11d0-94f2-00a0c91efb8b}        Deny_Write
+	Deny read access                    {53f56311-b6bf-11d0-94f2-00a0c91efb8b}          Deny_Read
+	Deny write access                   {53f56311-b6bf-11d0-94f2-00a0c91efb8b}          Deny_Write
 
 	CD and DVD:
-	Deny read access                    {53f56308-b6bf-11d0-94f2-00a0c91efb8b}        Deny_Read
-	Deny write access                   {53f56308-b6bf-11d0-94f2-00a0c91efb8b}        Deny_Write
+	Deny read access                    {53f56308-b6bf-11d0-94f2-00a0c91efb8b}          Deny_Read
+	Deny write access                   {53f56308-b6bf-11d0-94f2-00a0c91efb8b}          Deny_Write
 
 	Removable Disks:
-	Deny read access                    {53f5630d-b6bf-11d0-94f2-00a0c91efb8b}        Deny_Read
-	Deny write access                   {53f5630d-b6bf-11d0-94f2-00a0c91efb8b}        Deny_Write
+	Deny read access                    {53f5630d-b6bf-11d0-94f2-00a0c91efb8b}          Deny_Read
+	Deny write access                   {53f5630d-b6bf-11d0-94f2-00a0c91efb8b}          Deny_Write
 
 	Tape Drives:
-	Deny read access                    {53f5630b-b6bf-11d0-94f2-00a0c91efb8b}        Deny_Read
-	Deny write access                   {53f5630b-b6bf-11d0-94f2-00a0c91efb8b}        Deny_Write
+	Deny read access                    {53f5630b-b6bf-11d0-94f2-00a0c91efb8b}          Deny_Read
+	Deny write access                   {53f5630b-b6bf-11d0-94f2-00a0c91efb8b}          Deny_Write
 
 	WPD Devices:
-	Deny read access                    {6AC27878-A6FA-4155-BA85-F98F491D4F33}        Deny_Read
-	Deny write access                   {6AC27878-A6FA-4155-BA85-F98F491D4F33}        Deny_Write
-	Deny read access                    {F33FDC04-D1AC-4E8E-9A30-19BBD4B108AE}        Deny_Read
-	Deny write access                   {F33FDC04-D1AC-4E8E-9A30-19BBD4B108AE}        Deny_Write
+	Deny read access                    {6AC27878-A6FA-4155-BA85-F98F491D4F33}          Deny_Read
+	Deny write access                   {6AC27878-A6FA-4155-BA85-F98F491D4F33}          Deny_Write
+	Deny read access                    {F33FDC04-D1AC-4E8E-9A30-19BBD4B108AE}          Deny_Read
+	Deny write access                   {F33FDC04-D1AC-4E8E-9A30-19BBD4B108AE}          Deny_Write
 #>
 
 # Значение для параметра DWord.
@@ -32,7 +32,7 @@ $Value = 0
 try {
 	# Ветки в реестре.
 	$RegKey_0 = "HKLM:\Software\Policies\Microsoft\Windows"
-	$RegKey_1 = "HKLM:\Software\Policies\Microsoft\Windows\RemovableStorageDevices"	
+	$RegKey_1 = "HKLM:\Software\Policies\Microsoft\Windows\RemovableStorageDevices"
 	$RegKey_2 = "HKLM:\Software\Policies\Microsoft\Windows\RemovableStorageDevices\{53f56308-b6bf-11d0-94f2-00a0c91efb8b}"
 	$RegKey_3 = "HKLM:\Software\Policies\Microsoft\Windows\RemovableStorageDevices\{53f5630b-b6bf-11d0-94f2-00a0c91efb8b}"
 	$RegKey_4 = "HKLM:\Software\Policies\Microsoft\Windows\RemovableStorageDevices\{53f5630d-b6bf-11d0-94f2-00a0c91efb8b}"
@@ -55,85 +55,109 @@ $TestPath_6 = Test-Path $RegKey_6
 $TestPath_7 = Test-Path $RegKey_7
 
 # Создание веток в реестре.
-if (-Not($TestPath_1 -or $TestPath_2 -or $TestPath_3 -or $TestPath_4 -or $TestPath_5 -or $TestPath_6 -or $TestPath_7)) {
-	# Если ветки в реестре нет, то она создастся с присвоенным классом устройства "RemovableStorageDevices".
-	New-Item -Path $RegKey_0 -Name "RemovableStorageDevices" -Force | Out-Null
-	New-ItemProperty -Path $RegKey_1 -Name "Deny_All" -Value $Value -PropertyType 'DWord' -Force | Out-Null
-	New-ItemProperty -Path $RegKey_1 -Name "AllowRemoteDASD" -Value $Value -PropertyType 'DWord' -Force | Out-Null
+if (-Not($TestPath_1) -or -Not($TestPath_2) -or -Not($TestPath_3) -or -Not($TestPath_4) -or -Not($TestPath_5) -or -Not($TestPath_6) -or -Not($TestPath_7)) {
 
-	# Если ветки в реестре нет, то она создастся с присвоенным классом устройства "{53f56308-b6bf-11d0-94f2-00a0c91efb8b}".
-	New-Item -Path $RegKey_1 -Name "{53f56308-b6bf-11d0-94f2-00a0c91efb8b}" -Force | Out-Null
-	New-ItemProperty -Path $RegKey_2 -Name "Deny_Read" -Value $Value -PropertyType 'DWord' -Force | Out-Null
-	New-ItemProperty -Path $RegKey_2 -Name "Deny_Write" -Value $Value -PropertyType 'DWord' -Force | Out-Null
-	New-ItemProperty -Path $RegKey_2 -Name "Deny_Execute" -Value $Value -PropertyType 'DWord' -Force | Out-Null
+	$ArrayList = [System.Collections.ArrayList]@()
+	if (-Not($TestPath_1)) {
+		# Если ветки в реестре нет, то она создастся с присвоенным классом устройства "RemovableStorageDevices".
+		New-Item -Path $RegKey_0 -Name "RemovableStorageDevices" -Force | Out-Null
+		New-ItemProperty -Path $RegKey_1 -Name "Deny_All" -Value $Value -PropertyType 'DWord' -Force | Out-Null
+		New-ItemProperty -Path $RegKey_1 -Name "AllowRemoteDASD" -Value $Value -PropertyType 'DWord' -Force | Out-Null
+		[void]($ArrayList.Add($RegKey_1))
+	}
 
-	# Если ветки в реестре нет, то она создастся с присвоенным классом устройства "{53f5630b-b6bf-11d0-94f2-00a0c91efb8b}".
-	New-Item -Path $RegKey_1 -Name "{53f5630b-b6bf-11d0-94f2-00a0c91efb8b}" -Force | Out-Null
-	New-ItemProperty -Path $RegKey_3 -Name "Deny_Read" -Value $Value -PropertyType 'DWord' -Force | Out-Null
-	New-ItemProperty -Path $RegKey_3 -Name "Deny_Write" -Value $Value -PropertyType 'DWord' -Force | Out-Null
-	New-ItemProperty -Path $RegKey_3 -Name "Deny_Execute" -Value $Value -PropertyType 'DWord' -Force | Out-Null
+	if (-Not($TestPath_2)) {
+		# Если ветки в реестре нет, то она создастся с присвоенным классом устройства "{53f56308-b6bf-11d0-94f2-00a0c91efb8b}".
+		New-Item -Path $RegKey_1 -Name "{53f56308-b6bf-11d0-94f2-00a0c91efb8b}" -Force | Out-Null
+		New-ItemProperty -Path $RegKey_2 -Name "Deny_Read" -Value $Value -PropertyType 'DWord' -Force | Out-Null
+		New-ItemProperty -Path $RegKey_2 -Name "Deny_Write" -Value $Value -PropertyType 'DWord' -Force | Out-Null
+		New-ItemProperty -Path $RegKey_2 -Name "Deny_Execute" -Value $Value -PropertyType 'DWord' -Force | Out-Null
+		[void]($ArrayList.Add($RegKey_2))
+	}
 
-	# Если ветки в реестре нет, то она создастся с присвоенным классом устройства "{53f5630d-b6bf-11d0-94f2-00a0c91efb8b}".
-	New-Item -Path $RegKey_1 -Name "{53f5630d-b6bf-11d0-94f2-00a0c91efb8b}" -Force | Out-Null
-	New-ItemProperty -Path $RegKey_4 -Name "Deny_Read" -Value $Value -PropertyType 'DWord' -Force | Out-Null
-	New-ItemProperty -Path $RegKey_4 -Name "Deny_Write" -Value $Value -PropertyType 'DWord' -Force | Out-Null
-	New-ItemProperty -Path $RegKey_4 -Name "Deny_Execute" -Value $Value -PropertyType 'DWord' -Force | Out-Null
+	if (-Not($TestPath_3)) {
+		# Если ветки в реестре нет, то она создастся с присвоенным классом устройства "{53f5630b-b6bf-11d0-94f2-00a0c91efb8b}".
+		New-Item -Path $RegKey_1 -Name "{53f5630b-b6bf-11d0-94f2-00a0c91efb8b}" -Force | Out-Null
+		New-ItemProperty -Path $RegKey_3 -Name "Deny_Read" -Value $Value -PropertyType 'DWord' -Force | Out-Null
+		New-ItemProperty -Path $RegKey_3 -Name "Deny_Write" -Value $Value -PropertyType 'DWord' -Force | Out-Null
+		New-ItemProperty -Path $RegKey_3 -Name "Deny_Execute" -Value $Value -PropertyType 'DWord' -Force | Out-Null
+		[void]($ArrayList.Add($RegKey_3))
+	}
 
-	# Если ветки в реестре нет, то она создастся с присвоенным классом устройства "{53f56311-b6bf-11d0-94f2-00a0c91efb8b}".
-	New-Item -Path $RegKey_1 -Name "{53f56311-b6bf-11d0-94f2-00a0c91efb8b}" -Force | Out-Null
-	New-ItemProperty -Path $RegKey_5 -Name "Deny_Read" -Value $Value -PropertyType 'DWord' -Force | Out-Null
-	New-ItemProperty -Path $RegKey_5 -Name "Deny_Write" -Value $Value -PropertyType 'DWord' -Force | Out-Null
-	New-ItemProperty -Path $RegKey_5 -Name "Deny_Execute" -Value $Value -PropertyType 'DWord' -Force | Out-Null
+	if (-Not($TestPath_4)) {
+		# Если ветки в реестре нет, то она создастся с присвоенным классом устройства "{53f5630d-b6bf-11d0-94f2-00a0c91efb8b}".
+		New-Item -Path $RegKey_1 -Name "{53f5630d-b6bf-11d0-94f2-00a0c91efb8b}" -Force | Out-Null
+		New-ItemProperty -Path $RegKey_4 -Name "Deny_Read" -Value $Value -PropertyType 'DWord' -Force | Out-Null
+		New-ItemProperty -Path $RegKey_4 -Name "Deny_Write" -Value $Value -PropertyType 'DWord' -Force | Out-Null
+		New-ItemProperty -Path $RegKey_4 -Name "Deny_Execute" -Value $Value -PropertyType 'DWord' -Force | Out-Null
+		[void]($ArrayList.Add($RegKey_4))
+	}
 
-	# Если ветки в реестре нет, то она создастся с присвоенным классом устройства "{6AC27878-A6FA-4155-BA85-F98F491D4F33}".
-	New-Item -Path $RegKey_1 -Name "{6AC27878-A6FA-4155-BA85-F98F491D4F33}" -Force | Out-Null
-	New-ItemProperty -Path $RegKey_6 -Name "Deny_Read" -Value $Value -PropertyType 'DWord' -Force | Out-Null
-	New-ItemProperty -Path $RegKey_6 -Name "Deny_Write" -Value $Value -PropertyType 'DWord' -Force | Out-Null
-	New-ItemProperty -Path $RegKey_6 -Name "Deny_Execute" -Value $Value -PropertyType 'DWord' -Force | Out-Null
+	if (-Not($TestPath_5)) {
+		# Если ветки в реестре нет, то она создастся с присвоенным классом устройства "{53f56311-b6bf-11d0-94f2-00a0c91efb8b}".
+		New-Item -Path $RegKey_1 -Name "{53f56311-b6bf-11d0-94f2-00a0c91efb8b}" -Force | Out-Null
+		New-ItemProperty -Path $RegKey_5 -Name "Deny_Read" -Value $Value -PropertyType 'DWord' -Force | Out-Null
+		New-ItemProperty -Path $RegKey_5 -Name "Deny_Write" -Value $Value -PropertyType 'DWord' -Force | Out-Null
+		New-ItemProperty -Path $RegKey_5 -Name "Deny_Execute" -Value $Value -PropertyType 'DWord' -Force | Out-Null
+		[void]($ArrayList.Add($RegKey_5))
+	}
 
-	# Если ветки в реестре нет, то она создастся с присвоенным классом устройства "{F33FDC04-D1AC-4E8E-9A30-19BBD4B108AE}".
-	New-Item -Path $RegKey_1 -Name "{F33FDC04-D1AC-4E8E-9A30-19BBD4B108AE}" -Force | Out-Null
-	New-ItemProperty -Path $RegKey_7 -Name "Deny_Read" -Value $Value -PropertyType 'DWord' -Force | Out-Null
-	New-ItemProperty -Path $RegKey_7 -Name "Deny_Write" -Value $Value -PropertyType 'DWord' -Force | Out-Null
-	New-ItemProperty -Path $RegKey_7 -Name "Deny_Execute" -Value $Value -PropertyType 'DWord' -Force | Out-Null
+	if (-Not($TestPath_6)) {
+		# Если ветки в реестре нет, то она создастся с присвоенным классом устройства "{6AC27878-A6FA-4155-BA85-F98F491D4F33}".
+		New-Item -Path $RegKey_1 -Name "{6AC27878-A6FA-4155-BA85-F98F491D4F33}" -Force | Out-Null
+		New-ItemProperty -Path $RegKey_6 -Name "Deny_Read" -Value $Value -PropertyType 'DWord' -Force | Out-Null
+		New-ItemProperty -Path $RegKey_6 -Name "Deny_Write" -Value $Value -PropertyType 'DWord' -Force | Out-Null
+		New-ItemProperty -Path $RegKey_6 -Name "Deny_Execute" -Value $Value -PropertyType 'DWord' -Force | Out-Null
+		[void]($ArrayList.Add($RegKey_6))
+	}
 
-	echo ""
-	Write-Host "GOOD" -BackgroundColor Green -NoNewline
-	Write-Host " В реестре создались ветки" -ForegroundColor Cyan
-	echo ""
-	Write-Host "$($RegKey_1)"
-	Write-Host "Deny_All = $($Value)"
-	Write-Host "AllowRemoteDASD = $($Value)"
-	echo ""
-	Write-Host "$($RegKey_2)"
-	Write-Host "Deny_Read = $($Value)"
-	Write-Host "Deny_Write = $($Value)"
-	Write-Host "Deny_Execute = $($Value)"
-	echo ""
-	Write-Host "$($RegKey_3)"
-	Write-Host "Deny_Read = $($Value)"
-	Write-Host "Deny_Write = $($Value)"
-	Write-Host "Deny_Execute = $($Value)"
-	echo ""
-	Write-Host "$($RegKey_4)"
-	Write-Host "Deny_Read = $($Value)"
-	Write-Host "Deny_Write = $($Value)"
-	Write-Host "Deny_Execute = $($Value)"
-	echo ""
-	Write-Host "$($RegKey_5)"
-	Write-Host "Deny_Read = $($Value)"
-	Write-Host "Deny_Write = $($Value)"
-	Write-Host "Deny_Execute = $($Value)"
-	echo ""
-	Write-Host "$($RegKey_6)"
-	Write-Host "Deny_Read = $($Value)"
-	Write-Host "Deny_Write = $($Value)"
-	Write-Host "Deny_Execute = $($Value)"
-	echo ""
-	Write-Host "$($RegKey_7)"
-	Write-Host "Deny_Read = $($Value)"
-	Write-Host "Deny_Write = $($Value)"
-	Write-Host "Deny_Execute = $($Value)"
+	if (-Not($TestPath_7)) {
+		# Если ветки в реестре нет, то она создастся с присвоенным классом устройства "{F33FDC04-D1AC-4E8E-9A30-19BBD4B108AE}".
+		New-Item -Path $RegKey_1 -Name "{F33FDC04-D1AC-4E8E-9A30-19BBD4B108AE}" -Force | Out-Null
+		New-ItemProperty -Path $RegKey_7 -Name "Deny_Read" -Value $Value -PropertyType 'DWord' -Force | Out-Null
+		New-ItemProperty -Path $RegKey_7 -Name "Deny_Write" -Value $Value -PropertyType 'DWord' -Force | Out-Null
+		New-ItemProperty -Path $RegKey_7 -Name "Deny_Execute" -Value $Value -PropertyType 'DWord' -Force | Out-Null
+		[void]($ArrayList.Add($RegKey_7))
+	}
+
+	if ($ArrayList.Count -eq 1) {
+		echo ""
+		Write-Host "GOOD" -BackgroundColor Green -NoNewline
+		Write-Host " В реестре создалaсь веткa" -ForegroundColor Cyan
+		foreach($i in $ArrayList) {
+			if ($i -like $RegKey_1) {
+				Write-Host "$($RegKey_1)"
+				Write-Host "Deny_All = $($Value)"
+				Write-Host "AllowRemoteDASD = $($Value)"
+			}
+			elseif (($i -like $RegKey_2) -or ($i -like $RegKey_3) -or ($i -like $RegKey_4) -or ($i -like $RegKey_5) -or ($i -like $RegKey_6) -or ($i -like $RegKey_7)) {
+				echo ""
+				Write-Host "$($i)"
+				Write-Host "Deny_Read = $($Value)"
+				Write-Host "Deny_Write = $($Value)"
+				Write-Host "Deny_Execute = $($Value)"
+			}
+		}
+	}
+	else {
+		echo ""
+		Write-Host "GOOD" -BackgroundColor Green -NoNewline
+		Write-Host " В реестре создались ветки" -ForegroundColor Cyan
+		foreach($i in $ArrayList) {
+			if ($i -like $RegKey_1) {
+				Write-Host "$($RegKey_1)"
+				Write-Host "Deny_All = $($Value)"
+				Write-Host "AllowRemoteDASD = $($Value)"
+			}
+			elseif (($i -like $RegKey_2) -or ($i -like $RegKey_3) -or ($i -like $RegKey_4) -or ($i -like $RegKey_5) -or ($i -like $RegKey_6) -or ($i -like $RegKey_7)) {
+				echo ""
+				Write-Host "$($i)"
+				Write-Host "Deny_Read = $($Value)"
+				Write-Host "Deny_Write = $($Value)"
+				Write-Host "Deny_Execute = $($Value)"
+			}
+		}
+	}
 }
 # Изменение значений веток в реестре.
 else {
